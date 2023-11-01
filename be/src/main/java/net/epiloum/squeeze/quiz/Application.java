e33ada906f20f440723e2764ff3495a1e2e5afde
Jaewon Kim <epiloum@gmail.com>
2023-11-01 23:04:34 +0900

    add db connection information to spring boot

--- be/src/main/java/net/epiloum/squeeze/quiz/Application.java
+++ be/src/main/java/net/epiloum/squeeze/quiz/Application.java
@@ -2,8 +2,10 @@ package net.epiloum.squeeze.quiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/env.properties")
 public class Application {
 
 	public static void main(String[] args) {

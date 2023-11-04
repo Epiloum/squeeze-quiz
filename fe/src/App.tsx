78a6e82465f1b590c3497826bb55db31e45a0992
Jaewon Kim <epiloum@gmail.com>
2023-11-04 22:21:41 +0900

    make the page for signing in

---
+++ fe/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import "./typefaces/typefaces.css";
+import SignIn from './views/signin';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
